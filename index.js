769c49a446703ab0e7cf9e7c164bbd3d0dddb726
May Kim <mkim0514@gmail.com>
2018-06-13 04:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,20 +16,19 @@ function orderOfLine(index) {
 }
 
 function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return 'The line is currently empty.';
+  if (katzDeliLine.length != 0) {
+
+    var result = 'The line is currently: ';
+    for (var i = 0; i < katzDeliLine; i += 1) {
+      if (i === katzDeliLine.length - 1) {
+        result += orderOfLine(i);
+      } else {
+        result += `${orderOfLine(i)}, `
+      }
+    }
+
+    return result;
+  } else {
+  return 'The line is currently empty.';
   }
-  // if (katzDeliLine.length != 0) {
-  //
-  //   var result = 'The line is currently: ';
-  //   for (var i = 0; i < katzDeliLine; i += 1) {
-  //     if (i === katzDeliLine.length - 1) {
-  //       result += orderOfLine(i);
-  //     } else {
-  //       result += `${orderOfLine(i)}, `
-  //     }
-  //   }
-  //
-  //   return result;
-  //}
 }
