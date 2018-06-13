588a02a3b44ebcdbad5cd42f4ed1a661e83b35f8
May Kim <mkim0514@gmail.com>
2018-06-13 03:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'The line is currently empty.';
   }
-  if (katzDeliLine.length != 0) {
-
-    var result = 'The line is currently: ';
-    for (var i = 0; i < katzDeliLine; i += 1) {
-      if (i === katzDeliLine.length - 1) {
-        result += orderOfLine(i);
-      } else {
-        result += `${orderOfLine(i)}, `
-      }
-    }
-
-    return result;
+  // if (katzDeliLine.length != 0) {
+  //
+  //   var result = 'The line is currently: ';
+  //   for (var i = 0; i < katzDeliLine; i += 1) {
+  //     if (i === katzDeliLine.length - 1) {
+  //       result += orderOfLine(i);
+  //     } else {
+  //       result += `${orderOfLine(i)}, `
+  //     }
+  //   }
+  //
+  //   return result;
   }
 }
