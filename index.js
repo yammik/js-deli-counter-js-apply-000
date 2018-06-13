2a641607119f4e79d2f014e21927d81a38bf00cc
May Kim <mkim0514@gmail.com>
2018-06-13 03:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function takeANumber(katzDeliLine, person) {
 }
 
 function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
   var custServed = katzDeliLine.shift();
   return `Currently serving ${custServed}.`;
 }
